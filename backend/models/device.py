3923ad4ed53b21c2591a24cfeb6c8d51216c0cbd
Joel Sony <joelsonytab@gmail.com>
2026-01-25 18:06:54 +0530

    1. Added device model in /models

---
+++ backend/models/device.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Enum, BigInteger, TIMESTAMP, ForeignKey
+from core.database import Base
+
+class Device(Base):
+    __tablename__ = "devices"
+
+    device_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+
+    device_name = Column(String(255))
+    status = Column(Enum("ONLINE", "OFFLINE"), nullable=False, default="OFFLINE")
+    last_heartbeat = Column(TIMESTAMP, nullable=False)
+
+    storage_capacity = Column(BigInteger, nullable=False)
+    available_storage = Column(BigInteger, nullable=False)
+
