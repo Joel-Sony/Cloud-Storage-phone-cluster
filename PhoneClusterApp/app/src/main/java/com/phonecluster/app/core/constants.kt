9a288dbfeebff3aea03afa77a9dfa188e5bc75d6
Kevinjose102 <kevinjose2505@gmail.com>
2026-03-10 05:00:04 +0530

    fixed bugs, new ui and backend working properly

--- PhoneClusterApp/app/src/main/java/com/phonecluster/app/core/constants.kt
+++ PhoneClusterApp/app/src/main/java/com/phonecluster/app/core/constants.kt
@@ -1,4 +1,4 @@
 package com.phonecluster.app.core
 
-const val SERVER_BASE_URL = "http://10.90.84.130:8000"
-const val SERVER_IP = "10.90.84.130"
+const val SERVER_BASE_URL = "http://10.0.0.2:8000"
+const val SERVER_IP = "10.0.0.2"
